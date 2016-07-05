7a5d8ab6a3a3755e777ee3b6b4f6c70192f74c3f
yvkschaefer <yvkschaefer@gmail.com>
2016-07-05 18:04:47 +0000

    question 2

--- answers.js
+++ answers.js
@@ -3,6 +3,7 @@
 //Write a function that takes a string and returns the first character of the string. 
 //Test your function on a few inputs, including the empty string.
 
+/*
 var firstCharacter = function (string){
     return string.substring (0,1);
 }
@@ -11,3 +12,15 @@ console.log(firstCharacter("Kara"));
 console.log(firstCharacter("ara"));
 console.log(firstCharacter(""));
 console.log(firstCharacter());
+*/
+
+//Write a function that takes a string and returns the last character of a string. 
+//Test your function on a few inputs, including the empty string.
+
+var lastCharacter = function (string) {
+    return string.substring(string.length - 1, string.length);
+}
+
+console.log(lastCharacter("Super"));
+console.log(lastCharacter("Fun"));
+console.log(lastCharacter());
