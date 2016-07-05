2ff1d20afdaf00a6cd85456058d28905bd169aac
yvkschaefer <yvkschaefer@gmail.com>
2016-07-05 17:50:15 +0000

    first function

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
 // Hi, I'm a comment!
+
+//Write a function that takes a string and returns the first character of the string. 
+//Test your function on a few inputs, including the empty string.
+
+var firstCharacter = function (string){
+    return string.substring (0,1);
+}
+
+firstCharacter("Kara");
