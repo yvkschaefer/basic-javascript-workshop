4fca064ad78b1e307da7c60476b97fa927fb257a
yvkschaefer <yvkschaefer@gmail.com>
2016-07-05 18:22:03 +0000

    question 3

--- answers.js
+++ answers.js
@@ -14,9 +14,14 @@ console.log(firstCharacter(""));
 console.log(firstCharacter());
 */
 
+
+
+
+
 //Write a function that takes a string and returns the last character of a string. 
 //Test your function on a few inputs, including the empty string.
 
+/*
 var lastCharacter = function (string) {
     return string.substring(string.length - 1, string.length);
 }
@@ -24,3 +29,20 @@ var lastCharacter = function (string) {
 console.log(lastCharacter("Super"));
 console.log(lastCharacter("Fun"));
 console.log(lastCharacter());
+*/
+
+
+
+
+
+//Write a function that takes a string and a number, and returns the character at the position 
+//represented by the number. The indexing of number should start at 0. Test your function on a 
+//few inputs, including the empty string.
+
+function stringNumber(str, num) {
+    return str.substring(num - 1, num);
+}
+
+console.log(stringNumber("Face", 2));
+console.log(stringNumber("Seventeen", 6));
+console.log(stringNumber(""));
